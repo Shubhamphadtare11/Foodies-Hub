943646c599cf37e18b8b25a787ff9a9065d5910a
Shubham <shubham.phadtarez11@gmail.com>
2023-12-02 17:00:00 +0530

    .

--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -1,8 +1,6 @@
 const Grocery = () => {
     return(
-        <h1>
-            {" "}
-            This is online grocery store, there will be lots of child component inside this web page!!!</h1>
+        <h1>This is online grocery store, there will be lots of child component inside this web page!!!</h1>
     );
 };
 
