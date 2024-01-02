c85bff47db41893c1893fc770af95984d67b3c69
Shubham <shubham.phadtarez11@gmail.com>
2024-01-02 11:19:58 +0530

    https://proxy.cors.sh/

--- src/components/Body.js
+++ src/components/Body.js
@@ -19,9 +19,9 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
-
+//https://corsproxy.io/?
     const json = await data.json();
     // console.log(
     //   json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
