1307337829cc316cbe1bf8539875fb48562ff03c
Shubham <shubham.phadtarez11@gmail.com>
2024-01-02 12:04:51 +0530

    https://corsproxy.org/

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,
 
 export const LOGO_URL ="https://yt3.googleusercontent.com/ytc/AOPolaTYZvfB1HxdW8naFXuJ1ftxKiu2I-PnFoShyqHr=s900-c-k-c0x00ffffff-no-rj"
 
-export const MENU_API = "https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId="
+export const MENU_API = "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D12.9715987%26lng%3D77.5945627%26restaurantId%3D"
 
 export const MENU_ITEM_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit//"
 //https://corsproxy.io/?
